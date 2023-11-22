a13f710fdf44906768ead94d11b789e0e25abf67
forsterit <olegkrivolap57@gmail.com>
2023-11-22 12:23:00 +0300

    added edit

---
+++ salam/test1/bot.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from telebot import TeleBot
+dotenv_path = Path('salam/.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+bot = TeleBot(os.getenv('TELEGRAM_BOT_API_KEY'), threaded=False)
+
+
+
+def bot_save():
+    bot.send_message(chat_id='@channej_dj', text='Были добавлены данные')
+
+
+def bot_delete():
+    bot.send_message(chat_id='@channej_dj', text='Были удалены данные')
